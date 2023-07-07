8428df91f898804d1bf245995a018710cf62038e
Abiola Olamilekan <abiolawalter2015@gmail.com>
2023-07-07 16:44:38 +0100

    feat: implement css base config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,15 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    extend: {},
+    screens: {
+      md1200: { max: '1200px' },
+      md1000: { max: '1000px' },
+      min800: { max: '800px' },
+      min620: { max: '620px' },
+      min540: { max: '540px' },
+      min450: { max: '450px' },
+      min375: { max: '375px' },
     },
   },
   plugins: [],
